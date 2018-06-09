9723edf18bedd4fe1926907ab833b7998d5191ba
Kenny Lam <kennylam777@gmail.com>
2018-06-09 00:39:44 +0000

    Adding support for Xiaomi Mini, from bkye/rt-n56u

---
+++ trunk/configs/boards/XIAOMI-MINI/board.h
@@ -0,0 +1,32 @@
+/* Xiaomi Mi WiFi Mini */
+
+#define BOARD_PID		"XIAOMI-MINI"
+#define BOARD_NAME		"XIAOMI-MINI"
+#define BOARD_DESC		"Xiaomi Mi WiFi Mini"
+#define BOARD_VENDOR_NAME	"Xiaomi"
+#define BOARD_VENDOR_URL	"http://www.mi.com/"
+#define BOARD_MODEL_URL		"http://www.mi.com/hk/miwifimini/"
+#define BOARD_BOOT_TIME		25
+#define BOARD_FLASH_TIME	120
+#define BOARD_GPIO_BTN_RESET 30
+#undef BOARD_GPIO_BTN_WPS
+#undef  BOARD_GPIO_BTN_WLTOG
+#undef BOARD_GPIO_LED_ALL
+#undef BOARD_GPIO_LED_WIFI
+#undef  BOARD_GPIO_LED_SW2G
+#undef BOARD_GPIO_LED_SW5G
+#define BOARD_GPIO_LED_POWER	24
+#undef BOARD_GPIO_LED_LAN
+#undef BOARD_GPIO_LED_WAN
+#undef BOARD_GPIO_LED_USB
+#undef  BOARD_GPIO_LED_ROUTER
+#undef  BOARD_GPIO_PWR_USB
+#define BOARD_HAS_5G_11AC	1
+#define BOARD_NUM_ANT_5G_TX	2
+#define BOARD_NUM_ANT_5G_RX	2
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	1
+#define BOARD_HAS_EPHY_L1000	0
+#define BOARD_HAS_EPHY_W1000	0
+
